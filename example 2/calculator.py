cd3f8908be98b5fc9d4b115a7a1fc034b9d924c1
TinaNanzala <47854016+TinaNanzala@users.noreply.github.com>
2019-04-02 20:58:53 +0300

    Add files via upload

---
+++ example 2/calculator.py
@@ -0,0 +1,36 @@
+def add (b,c):
+	d = b+c
+	print(d)
+
+def subtract (b,c):
+	d = b-c
+	print (d)
+
+def multiply (b,c):
+	d = b*c
+	print (d)
+
+def division (b,c):
+	d = b/c
+	print (d)
+
+def modulus (b,c):
+	d = b%c
+	print (d)
+
+
+def percentage (b,c):
+     d = b*c/100
+     print (d)
+
+def division(b,c):
+     d = b//c
+     print (d)
+
+def exponent (b,c):
+	d = b**c
+	print (d)
+
+def float (b,c):
+	d = b/c
+	print (d)
