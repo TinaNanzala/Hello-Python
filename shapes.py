5fae6d51a581e6531b5c1b6fc2902ff77d698384
TinaNanzala <47854016+TinaNanzala@users.noreply.github.com>
2019-04-30 19:35:24 +0300

    Add files via upload
    code 1

---
+++ shapes.py
@@ -0,0 +1,57 @@
+class Circle:
+	def__init__(self,radius)
+	self.radius = radius
+
+	def Area(self,Area):
+		self.radius=radius
+		Area(3.14)*(radius*radius)
+		print(Area)
+
+
+	def circumference(self):
+		self.radius=radius
+		for area in area:
+		circumference=2*(3.14)*radius
+		print(circumference)
+class Square:
+	def __init__(self,side);
+		self.side=side
+
+    def Area(self,Area)
+    	self.side=side
+    	for area in area:
+		Area=a(A)
+		print(Area)
+
+	def perimeter(self,perimeter)
+		self.side=side
+	    perimeter=4*a(p)
+	    for b in perimeter:
+	    	print(b)
+class Rectangle:
+	def__init__(self,w,l)
+		self.Area=Area
+		self.perimeter=perimeter
+
+	def area(self,area)
+		Area=(W)*L
+		for c in width;
+		print(c)
+	def perimeter(self,perimeter)
+		p=2(L+W)
+		for d in p;
+		print(p)
+class Sphere:
+	def__init__(self,radius,surface area,volume)
+	self.radius=radius
+	self.surface area=surafce area
+	self.volume=volume
+	def Area (self,Area)
+	    Area=4r(3.14)
+	    for d in Area:
+	    	print(d)
+
+	
+
+
+
