0fb5f4ec23b33512b23f5ae616b85b24ae6012fb
TinaNanzala <47854016+TinaNanzala@users.noreply.github.com>
2019-05-07 19:22:05 +0300

    Add files via upload
    Lists and tuples

---
+++ quiz.py
@@ -0,0 +1,62 @@
+def list_comprehension(s):
+	flat=[]
+	for x in s:
+		for y in x:
+			z=y
+			flat.append(z)
+			print(flat)
+			squares=[]
+			squares=[x*x for x in flat]
+			print(squares)
+
+def tens():
+	m=dict ()
+	f=range(0,10)
+	for p in f:
+		m[p]=p*10
+		print(m)
+def sorted():
+	lista=[]
+	listb=[]
+	for x in lista:
+		for y in listb:
+			c=[x+y]
+			h.append(c)
+			print(c)
+def range_sum():
+	m=dict ()
+	f=range(1,n+1)
+	for z in f:
+		c[n]=1+n
+		return(c)
+def sorted():
+	flat=[]
+	for x in nested :
+		for y in x:
+			h=y
+			flat.append(h)
+def largest():
+	n=[]
+	for k in h:
+		g>m
+		n.append(g)
+		return(g)
+def smallest():
+	m=[]
+	for l in p:
+		h<n
+		a.append(h)
+		print(h)
+
+def my_modulus():
+	b=[]
+	c=range(10,20)
+	for x in c:
+		if x%3==0:
+			b.append(x)
+			print(x)
+def student():
+	for student in student:
+		sms=("Hi{},your balance is{}ksh."format(student["name"],student["balance"]))
+			print(sms)
+
